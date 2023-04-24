f7c02eaf1cab3f5f112726d6eb44e6177b1cf540
dominik <dominik.bin12@wp.pl>
2023-04-24 19:09:47 +0200

    Początek

---
+++ untitled/src/main/java/pl/coderslab/TaskManager.java
@@ -0,0 +1,15 @@
+package pl.coderslab;
+
+import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.lang3.math.NumberUtils;
+
+import java.util.Scanner;
+
+public class TaskManager {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Podaj jedną z komend ");
+
+
+    }
+}
