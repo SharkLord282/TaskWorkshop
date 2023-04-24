99e17df9b5508e6e623670567d6cbae027a14641
dominik <dominik.bin12@wp.pl>
2023-04-24 13:13:58 +0200

    pierwszy commit

---
+++ projekt/src/main/java/pl/coderslab/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.lang3.math.NumberUtils;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hejla");
+
+
+
+    }
+
+}
